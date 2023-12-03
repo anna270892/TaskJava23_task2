0db6933165d12037d717f1a3e25edcb54f6fc29c
Anna Kochergina <akochergina92@gmail.com>
2023-12-03 20:36:36 +0300

    refactors3

---
+++ src/main/java/RegistrationDto.java
@@ -0,0 +1,88 @@
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class RegistrationDto {
+    private String login;
+    private String password;
+    private String status;
+
+    public RegistrationDto(String login, String password, String status) {
+        this.login = login;
+        this.password = password;
+        this.status = status;
+    }
+
+    static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(7777)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    static RegistrationDto request(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+        return user;
+    }
+
+    private static final Faker faker = new Faker(new Locale("ru"));
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            return request(getUser(status));
+        }
+    }
+
+    // Геттеры
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // Сеттеры
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,110 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AuthTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:7777/");
+    }
+
+    //успешный вход в личный кабинет
+    @Test
+    void activeRegisteredUser() {
+        var registeredUser = RegistrationDto.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(text("Личный кабинет")).shouldBe(visible);
+    }
+
+    //не успешный вход в личный кабинет
+    @Test
+    void notActiveRegisteredUser() {
+        var notRegisteredUser = RegistrationDto.Registration.getUser("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(visible);
+    }
+
+    //заблокированный пользователь
+    @Test
+    void ShouldGetErrorMessageIfLoginWithBlockedRegisteredUser() {
+        var blockedUser = RegistrationDto.Registration.getRegisteredUser("blocked");
+        var wrongPassword = RegistrationDto.getRandomPassword();
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(text("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(11))
+                .shouldBe(visible);
+    }
+
+    //невалидный логин
+    @Test
+    void notValidLogin() {
+        var registeredUser = RegistrationDto.Registration.getRegisteredUser("active");
+        var wrongLogin = RegistrationDto.getRandomLogin();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(11))
+                .shouldBe(visible);
+    }
+
+    //пустое поле "Логин"
+    @Test
+    void emptyFieldLogin() {
+        var registeredUser = RegistrationDto.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] input").clear();
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id='login'] .input__sub").shouldHave(text("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    //невалидный пароль
+    @Test
+    void notValidPassword() {
+        var registeredUser = RegistrationDto.Registration.getRegisteredUser("active");
+        var wrongPassword = RegistrationDto.getRandomPassword();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $("button.button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(text("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(11))
+                .shouldBe(visible);
+    }
+
+    //пустое поле "Пароль"
+    @Test
+    void emptyFieldPassword() {
+        var registeredUser = RegistrationDto.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").clear();
+        $("button.button").click();
+        $("[data-test-id='password'] .input__sub").shouldHave(text("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    //отправка пустой формы
+    @Test
+    void emptyFieldLoginAndPassword() {
+        var registeredUser = RegistrationDto.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] input").clear();
+        $("[data-test-id='password'] input").clear();
+        $("button.button").click();
+        $("[data-test-id='login'] .input__sub").shouldHave(text("Поле обязательно для заполнения")).shouldBe(visible);
+        $("[data-test-id='password'] .input__sub").shouldHave(text("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+}
+
